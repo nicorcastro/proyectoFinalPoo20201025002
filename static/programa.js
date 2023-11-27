8a5f0725eedc7c629430cf58dc47ea381a6f7899
Nicolás Rojas Castro <nicoroca.97@hotmail.com>
2023-11-27 16:18:29 -0500

    Organizando página principal

--- static/programa.js
+++ static/programa.js
@@ -1,6 +1,8 @@
-var map = L.map('map').setView([4.873540409359415, -74.14572792301244], 13); 
+var map = L.map('map').setView([4.6416912, -74.0735232], 16); 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
 
